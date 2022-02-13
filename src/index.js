36c0cdadfb893e267ac1711ecf4191b51e160ee1
DESKTOP-274R2DT\Justin <justinc.s.wong@gmail.com>
2022-02-13 14:08:18 -0800

    remove package-lock file and add prod server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://serene-bayou-82639.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
